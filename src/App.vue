f8a3c7ccf62cadceb933f65e2f516af6a9f29292
Sebin Song <songxevin@gmail.com>
2018-12-28 14:25:45 +0900

    SASS loader installed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <p id="sebin">Sebin Song is doing an experiment with SCSS</p>
   </div>
 </template>
-
+<style lang="scss">
+  @import './assets/scss/App.scss';
+</style>
 <script>
 import HelloWorld from './components/HelloWorld'
 
@@ -15,14 +16,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
